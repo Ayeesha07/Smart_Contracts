dd7b55aae625d5ec708356ac95bf775bdc8e02a5
Ayeesha07 <ayeeshabepari7@gmail.com>
2025-12-08 22:16:44 +0530

    Update SimpleToken.sol

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract SimpleToken {
+    string public name = "DemoToken";
+    string public symbol = "DMT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply * 10**uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "not enough balance");
+        require(_to != address(0), "invalid address");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
